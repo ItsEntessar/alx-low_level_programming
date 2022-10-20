067bad3e30266311eff36f56703c60313f86fd69
ItsEntessar <entessaribrahim@gmail.com>
2022-10-20 14:45:58 -0700

    print3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,14 @@
 */
 
 void print_numbers(void)
+
 {
 
-int a = 0;
-while (a < 10)
-_putchar(a++ + '0')
+	int a = 0;
 
-_putchar('\n');
+	while (a < 10)
+		_putchar(a++ + '0');
+
+			_putchar('\n');
 
 }
