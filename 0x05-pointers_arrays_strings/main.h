e62b240bb3f3ce30b17e597a56c501648a2d88ab
ItsEntessar <entessaribrahim@gmail.com>
2022-10-24 16:09:59 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,9 +8,10 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts(char *str);
+void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int *n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+
 #endif
